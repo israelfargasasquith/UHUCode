d2b986af7b6bcc4a681713123aea6262360f1b00
israelfargasasquith <124618058+israelfargasasquith@users.noreply.github.com>
2023-10-17 18:30:57 +0200

    Práctica 1 Mia aparte
    Voy a seguir trabajando con Marina y Miriam pero quiero hacer la P1 a mi manera para la defensa

---
+++ AMC/Práctica 1/AMC Práctica 1/src/amc/práctica/pkg1/AMCPráctica1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package amc.práctica.pkg1;
+
+/**
+ *
+ * @author 34667
+ */
+public class AMCPráctica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
