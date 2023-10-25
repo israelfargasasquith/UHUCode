b70880e2474bbe6ab4be6e8270f023f457c7f3e0
israelfargasasquith <124618058+israelfargasasquith@users.noreply.github.com>
2023-10-25 19:59:48 +0200

    Práctica 1 empezada portátil

---
+++ DDSI/Práctica 1/DDSIPractica1/src/main/java/APP/DDSIPractica1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package APP;
+
+/**
+ *
+ * @author 34667
+ */
+public class DDSIPractica1 {
+
+    public static void main(String[] args) {
+        ControladorLo
+    }
+}
