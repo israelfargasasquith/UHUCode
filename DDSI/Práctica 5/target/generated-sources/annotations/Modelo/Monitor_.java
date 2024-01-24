7c5dcad80a89542314b52a1dc9df24f531710696
israelfargasasquith <124618058+israelfargasasquith@users.noreply.github.com>
2024-01-24 13:30:53 +0100

    Add files via upload

---
+++ DDSI/Práctica 5/target/generated-sources/annotations/Modelo/Monitor_.java
@@ -0,0 +1,22 @@
+package Modelo;
+
+import Modelo.Actividad;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-01-09T17:22:14", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Monitor.class)
+public class Monitor_ { 
+
+    public static volatile SingularAttribute<Monitor, String> nick;
+    public static volatile SingularAttribute<Monitor, String> codMonitor;
+    public static volatile SingularAttribute<Monitor, String> correo;
+    public static volatile SetAttribute<Monitor, Actividad> actividadesResponsable;
+    public static volatile SingularAttribute<Monitor, String> fechaEntrada;
+    public static volatile SingularAttribute<Monitor, String> telefono;
+    public static volatile SingularAttribute<Monitor, String> nombre;
+    public static volatile SingularAttribute<Monitor, String> dni;
+
+}
---
+++ DDSI/Práctica 5/target/generated-sources/annotations/Modelo/Socio_.java
@@ -0,0 +1,23 @@
+package Modelo;
+
+import Modelo.Actividad;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-01-09T17:22:14", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Socio.class)
+public class Socio_ { 
+
+    public static volatile SingularAttribute<Socio, String> numeroSocio;
+    public static volatile SingularAttribute<Socio, String> fechaNacimiento;
+    public static volatile SingularAttribute<Socio, String> correo;
+    public static volatile SingularAttribute<Socio, Character> categoria;
+    public static volatile SingularAttribute<Socio, String> fechaEntrada;
+    public static volatile SingularAttribute<Socio, String> telefono;
+    public static volatile SetAttribute<Socio, Actividad> actividades;
+    public static volatile SingularAttribute<Socio, String> nombre;
+    public static volatile SingularAttribute<Socio, String> dni;
+
+}
